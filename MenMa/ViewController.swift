0ac6ca67878f212ce4ab3992627e0a42e3eff2e2
Keita Ito <keitaitok@gmail.com>
2016-01-06 14:20:12 -0800

    Convert Swift syntax to latest

--- MenMa/ViewController.swift
+++ MenMa/ViewController.swift
@@ -20,16 +20,16 @@ class ViewController: UIViewController {
         url = NSURL(string: "https://api.foursquare.com/v2/venues/search?ll=37.7992426,-122.4007343&query=ramen&oauth_token=REZLGOWAE45WNZ21NHBSNUBNOJXC32AQYNFJOQEB0SLDPTQP&v=20150613")
         let request = NSURLRequest(URL: url!)
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
             
-            var serializedJSON: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+            let serializedJSON: NSDictionary = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
             
-            var venueArray = serializedJSON["response"]!["venues"] as! NSArray
+            let venueArray = serializedJSON["response"]!["venues"] as! NSArray
             
             for venue in venueArray {
-                var name = venue["name"]
+                let name = venue["name"]
                 if let n = name as? NSString {
-                    println(n)
+                    print(n)
                     self.cachedRamenNames.append(n as String)
                 }
             }
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
             self.sharedDefaults.setObject(self.cachedRamenNames, forKey: "ramenPlaceNames")
             self.sharedDefaults.synchronize()
             
-            println(self.cachedRamenNames)
+            print(self.cachedRamenNames)
         }
     }
 
