6d22d42eaea330b5be8ef8b929c97145017a9d93
Keita Ito <keitaitok@gmail.com>
2016-01-18 12:10:11 -0800

    Add NetworkManager and MainViewController

---
+++ MenMa/Managers/NetworkManager.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkManager.swift
+//  MenMa
+//
+//  Created by Keita Ito on 1/16/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct NetworkManager {
+    
+    let manager = Alamofire.Manager.sharedInstance
+    
+    func download(url url: URLStringConvertible, completion: (results: Array<Venue>) -> Void) -> Void {
+        
+        var resultsArray: Array<Venue> = Array<Venue>()
+        
+        manager.request(.GET, url).responseJSON { response in
+            // Parse JSON data.
+            let responseResultValue: Dictionary? = response.result.value as? Dictionary<String, AnyObject>
+            if let responseDictionary = responseResultValue?["response"] as? Dictionary<String, AnyObject> {
+                if let venues = responseDictionary["venues"] as? Array<AnyObject> {
+                    print("Venus count: \(venues.count)")
+                    
+                    // Iterate venues array of JSON data.
+                    for venue in venues {
+                        if let venue = venue as? Dictionary<String, AnyObject> {
+                            guard let name = venue["name"] as? String else { return }
+                            guard let id = venue["id"] as? String else { return }
+                            let url = venue["url"] as? String
+                            
+                            // Instantiate objects of type Venue struct.
+                            let aVenueStruct = Venue(name: name, id: id, url: url)
+                            resultsArray.append(aVenueStruct)
+                        }
+                    }
+                    // Completion handler. Pass resultsArray to the caller object.
+                    completion(results: resultsArray)
+                }
+            }
+        }
+    }
+}
+
+struct Venue {
+    let name: String
+    let id: String
+    let url: URLStringConvertible?
+//    let location
+//    let menu
+//    let contact
+    
+    init(name: String, id: String, url: String?) {
+        self.name = name
+        self.id = id
+        self.url = url
+    }
+}
---
+++ MenMa/ViewControllers/MainViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainViewController.swift
+//  MenMa
+//
+//  Created by Keita Ito on 1/16/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class MainViewController: UIViewController {
+    
+    let url: URLStringConvertible = "https://api.foursquare.com/v2/venues/search?ll=37.7992426,-122.4007343&query=ramen&oauth_token=REZLGOWAE45WNZ21NHBSNUBNOJXC32AQYNFJOQEB0SLDPTQP&v=20150613"
+    let manager = NetworkManager()
+    
+    var venues: Array<Venue> = Array<Venue>() {
+        didSet {
+            print(self.venues)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        manager.download(url: self.url) { (results) -> Void in
+            self.venues = results
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
